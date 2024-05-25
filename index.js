bd0305f0a9d11beb131b52276785069c012403e4
IVANJULES <ivan.julesdeguzman7@gmail.com>
2024-05-25 15:26:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const countLikes1 = document.getElementById("countLikes1")
 function clickLike1(){
     let totalLikes1 = parseInt(countLikes1.value) + 1
     countLikes1.textContent = totalLikes1.toString()
+
+
 }
 btnLike1.addEventListener("click",clickLike1)
